9a28b3853f6bddbeb46d8903a10566b307bf8ebc
xnerhu <xnerhu@gmail.com>
2023-02-04 15:44:15 +0100

    refactor: use vec insteadd of vecdequeue in recursive_lifo

--- packages/lib/src/components/lifo/recursive_lifo.rs
+++ packages/lib/src/components/lifo/recursive_lifo.rs
@@ -5,7 +5,8 @@ use crate::components::component_context::ComponentContext;
 pub struct RecursiveLIFO {
     ctx: ComponentContext,
     size: usize,
-    values: VecDeque<Option<f64>>,
+    values: Vec<Option<f64>>,
+    // values: VecDeque<Option<f64>>,
     pub is_filled: bool,
 }
 
@@ -16,26 +17,29 @@ impl RecursiveLIFO {
         return RecursiveLIFO {
             ctx,
             size,
-            values: VecDeque::new(),
+            // values: VecDeque::new(),
+            values: Vec::new(),
             is_filled: false,
         };
     }
 
-    pub fn at(&mut self, index: usize) -> &Option<f64> {
-        self.values.make_contiguous();
-        let index = self.size - 1 - index;
+    pub fn get(&mut self, index: usize) -> &Option<f64> {
+        let index = (self.values.len() - 1) - index;
         return self.values.get(index).unwrap();
     }
 
     pub fn values(&mut self) -> &[Option<f64>] {
-        self.values.make_contiguous();
-        return self.values.as_slices().0;
+        let start_index = self.values.len() - (self.size - 1);
+        return &self.values[start_index..];
+        // self.values.make_contiguous();
+        // return self.values.as_slices().0;
     }
 
     pub fn next(&mut self, value: Option<f64>) -> (Option<f64>, Option<f64>, bool) {
         self.ctx.assert();
         let mut first_value: Option<f64> = None;
-        self.values.push_front(value);
+        // self.values.push_front(value);
+        self.values.push(value);
 
         let len = self.values.len();
 
@@ -43,7 +47,10 @@ impl RecursiveLIFO {
             self.is_filled = true;
 
             if len > 1 {
-                first_value = self.values.pop_back().unwrap();
+                first_value = *self.get(self.size - 1);
+                // let index = len - (self.size - 1);
+                // first_value = self.values.get(index).unwrap().clone();
+                // first_value = self.values.pop_back().unwrap();
             }
         }
 
